5471f2ee0a34de434c313e10f0ca6b5d7e7d3ebd
Amrendra Singh <happysingh0987688@gmail.com>
2025-11-12 16:01:23 +0530

    solved

---
+++ Rating_800/1900A.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define loop(i, a, b) for (int i = (a); i < (b); i++)
+#define rloop(i, a, b) for (int i = (a); i >= (b); i--)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e8;
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll modpow(ll a, ll b, ll mod = MOD)
+{
+    ll res = 1;
+    while (b)
+    {
+        if (b & 1)
+            res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    bool flag = false;
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (s[i] == '.' && s[i + 1] == '.' && s[i + 2] == '.')
+        {
+            flag = true;
+            break;
+        }
+    }
+    if (flag)
+        cout << 2;
+    else
+    {
+        int cnt = 0;
+        for (char c : s)
+            if (c == '.')
+                cnt++;
+
+        cout << cnt;
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    fast_io;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
